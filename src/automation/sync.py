145ff80fed43ceb33ea39a780f318c1f914ccb4d
Dustin Farris <dustin@dustinfarris.com>
2013-01-24 21:54:52 -0500

    'sync' should merge from the production branch

--- src/automation/sync.py
+++ src/automation/sync.py
@@ -4,7 +4,7 @@ from fabric.api import *
 @task(default=True)
 def sync():
   current_branch = local("git branch | grep '*'", capture=True)[2:]
-  if current_branch == env.staging_branch:
-    abort("You are on the %s branch!" % env.staging_branch)
-  local("git merge --no-ff --no-edit origin/%s" % env.staging_branch)
+  if current_branch == env.production_branch:
+    abort("You are on the %s branch!" % env.production_branch)
+  local("git merge --no-ff --no-edit origin/%s" % env.production_branch)
   local("git push origin %s" % current_branch)
