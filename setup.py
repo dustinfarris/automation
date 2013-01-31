01c85cb6d387b726dbbc7935e6f1c97649347a89
Dustin Farris <dustin@dustinfarris.com>
2013-01-30 22:03:53 -0500

    Version 0.3.3.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 
 setup(
     name='automation',
-    version='0.3.3.1',
+    version='0.3.3.2',
     author='Dustin Farris',
     author_email='dustin@dustinfarris.com',
     url='https://github.com/dustinfarris/automation',
