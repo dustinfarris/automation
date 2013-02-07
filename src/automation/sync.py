939b26fc915dfd21bf07accf704dbef5cace9137
Dustin Farris <dustin@dustinfarris.com>
2013-02-07 13:32:25 -0500

    fetch origin before syncing

--- src/automation/sync.py
+++ src/automation/sync.py
@@ -6,5 +6,6 @@ def sync():
     current_branch = local("git branch | grep '*'", capture=True)[2:]
     if current_branch == env.production_branch:
         abort("You are on the %s branch!" % env.production_branch)
+    local("git fetch origin")
     local("git merge --no-ff --no-edit origin/%s" % env.production_branch)
     local("git push origin %s" % current_branch)
