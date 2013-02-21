e09e53edad7d7dd412fa04d2502e23014b67f77e
Dustin Farris <dustin@dustinfarris.com>
2013-02-21 18:49:15 -0500

    Checkout production branch before starting a new topic

--- src/automation/topic.py
+++ src/automation/topic.py
@@ -5,5 +5,6 @@ from fabric.api import *
 def new(branch_name=None):
     branch_name = branch_name or prompt(
         "New branch name:", validate=r'^[a-z0-9_-]{3,20}$')
+    local("git checkout %s" % env.production_branch)
     local("git checkout -b %s" % branch_name)
     local("git push -u origin %s" % branch_name)
