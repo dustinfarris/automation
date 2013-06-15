a19c8eb3c7a2dd0c83a741a8c18aea0f1951edb1
Dustin Farris <dustin@dustinfarris.com>
2013-06-15 17:59:48 -0400

    Version 0.5.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 
 setup(
     name='automation',
-    version='0.5.3',
+    version='0.5.4',
     author='Dustin Farris',
     author_email='dustin@dustinfarris.com',
     url='https://github.com/dustinfarris/automation',
